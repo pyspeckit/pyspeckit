3c43ef31acf8dd301f595accf0c40f7c7d0a47a9
"Adam Ginsburg ext:(%22) <keflavich@gmail.com>
2011-03-06 15:22:26 -0700

    added setp.yp and hgignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='spectrum',
+      version='0.1',
+      description='Spectroscopic Toolkit for fitting and manipulating spectroscopic data in python',
+      author=['Adam Ginsburg','Jordan Mirocha'],
+      author_email=['adam.g.ginsburg@gmail.com','mirochaj@gmail.com'],
+      url='https://bitbucket.org/keflavich/spectroscopic-toolkit-astronomy/',
+      packages=['spectrum'],
+     )
