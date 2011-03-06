5bfdda01aeaa75930240fcaf88d0590bdfd0b46f
"Adam Ginsburg ext:(%22) <keflavich@gmail.com>
2011-03-06 16:20:21 -0700

    modified text in test_fits

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -7,8 +7,11 @@ sp.plotter()
 print "How about now? ",sp.plotter.axis
 
 
+# set the baseline to zero to prevent variable-height fitting
+# (if you don't do this, the best fit to the spectrum is dominated by the
+# background level)
 sp.baseline.order = 0
-sp.specfit(quiet=False)
+sp.specfit()
 print "Guesses: ", sp.specfit.guesses
 print "Best fit: ", sp.specfit.modelpars
 
