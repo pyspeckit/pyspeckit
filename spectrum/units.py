3493a254a2fc640738c3578de2936a688036a952
"Adam Ginsburg ext:(%22) <keflavich@gmail.com>
2011-03-07 06:32:49 -0700

    added units conversion tool (empty)

---
+++ spectrum/units.py
@@ -0,0 +1,10 @@
+"""
+Unit parsing and conversion tool
+"""
+
+
+length_dict = {'meters':1.0,'m':1.0
+        'centimeters':1e-2,'cm':1e-2,
+        'millimeters':1e-3,'mm':1e-3,
+        'kilometers':1e3,'km':1e3,
+        }
