Fixed minor errors scattered throughout plotting, baselining routines, primarily having to do with order of operations and setting limits, but also some figure stuff and unit conversion fixes.
Ammonia model allows fitting for tau, but the labeling hasn't been fixed yet.
There's still an issue with forcing Tex < Tkin

spectrum/baseline.py
@@ -184,7 +184,9 @@ class Baseline:
             self.bx1 = np.argmin(abs(self.specplotter.xmin-self.Spectrum.xarr))
             self.bx2 = np.argmin(abs(self.specplotter.xmax-self.Spectrum.xarr))
         else:
-            raise ValueError("Need to input xmin and xmax, or have them set by plotter, for selectregion.")
+            self.bx1 = 0
+            self.bx2 = self.Spectrum.data.shape[0]
+            #raise ValueError("Need to input xmin and xmax, or have them set by plotter, for selectregion.")
         if self.bx1>self.bx2: self.bx1,self.bx2 = self.bx2,self.bx1
         if highlight:
             self.fitregion += self.specplotter.axis.plot(

spectrum/fitters.py
@@ -214,16 +214,18 @@ class Specfit(object):
                 npeaks=self.npeaks,
                 params=self.guesses,
                 **self.fitkwargs)
+        if model is None:
+            raise ValueError("Model was not set by fitter.  Examine your fitter.")
         self.chi2 = chi2
         self.dof  = self.gx2-self.gx1-self.npeaks*npars[fittype]
         self.model = model
         self.modelpars = mpp.tolist()
         self.modelerrs = mpperr.tolist()
+        self.residuals = self.spectofit[self.gx1:self.gx2] - self.model
         if self.specplotter.axis is not None:
             self.plot_fit()
-        self.residuals = self.spectofit[self.gx1:self.gx2] - self.model
-        if self.autoannotate:
-            self.annotate()
+            if self.autoannotate:
+                self.annotate()
     
     def peakbgfit(self, usemoments=True, annotate=True, vheight=True, height=0,
             negamp=None, fittype='gaussian', **kwargs):
@@ -253,6 +255,8 @@ class Specfit(object):
                 vheight=vheight,
                 params=self.guesses,
                 **self.fitkwargs)
+        if model is None:
+            raise ValueError("Model was not set by fitter.  Examine your fitter.")
         self.chi2 = chi2
         self.dof  = self.gx2-self.gx1-self.npeaks*npars[fittype]-vheight
         if vheight: 
@@ -265,9 +269,9 @@ class Specfit(object):
         self.modelerrs = mpperr[1:].tolist()
         if self.specplotter.axis is not None:
             self.plot_fit()
-        if annotate:
-            self.annotate()
-            if vheight: self.Spectrum.baseline.annotate()
+            if annotate:
+                self.annotate()
+                if vheight: self.Spectrum.baseline.annotate()
 
     def plot_fit(self):
         if self.Spectrum.baseline.subtracted is False and self.Spectrum.baseline.basespec is not None:
@@ -364,7 +368,9 @@ class Specfit(object):
             self.gx1 = np.argmin(abs(self.specplotter.xmin-self.Spectrum.xarr))
             self.gx2 = np.argmin(abs(self.specplotter.xmax-self.Spectrum.xarr))
         else:
-            raise ValueError("Need to input xmin and xmax, or have them set by plotter, for selectregion.")
+            self.gx1 = 0
+            self.gx2 = self.Spectrum.data.shape[0]
+            #raise ValueError("Need to input xmin and xmax, or have them set by plotter, for selectregion.")
 
         if self.gx1 == self.gx2:
             # Reset if there is no fitting region

spectrum/models/ammonia.py
@@ -126,6 +126,13 @@ class ammonia_model(object):
                 (1-np.exp(-h*freq_dict[linename]/(kb*tex))) /
                 (width/ckms*freq_dict[linename]*np.sqrt(2*np.pi)) )
       
+        # allow tau11 to be specified instead of Ntot
+        if tau11 is not None:
+            tau11_temp = tau_dict['oneone']
+            for linename,tau in tau_dict.iteritems():
+                tau_dict[linename] = tau * tau11/tau11_temp
+
+        for linename in line_names:
             voff_lines = np.array(voff_lines_dict[linename])
             tau_wts = np.array(tau_wts_dict[linename])
       
@@ -137,7 +144,7 @@ class ammonia_model(object):
             # tau array
             tauprof = np.zeros(len(xarr))
             for kk,no in enumerate(nuoff):
-              tauprof += tau_dict[linename]*tau_wts[kk]*\
+                tauprof += tau_dict[linename]*tau_wts[kk]*\
                         np.exp(-(xarr+no-lines[kk])**2/(2*nuwidth[kk]**2))
       
             T0 = (h*xarr*1e9/kb)
@@ -147,7 +154,7 @@ class ammonia_model(object):
       
         return runspec
 
-    def n_ammonia(self, pars=None,**kwargs):
+    def n_ammonia(self, pars=None, fittau=False, **kwargs):
         """
         Returns a function that sums over N ammonia line profiles, where N is the length of
         tkin,tex,Ntot,width,xoff_v,fortho *OR* N = len(pars) / 6
@@ -167,10 +174,18 @@ class ammonia_model(object):
         elif not(len(tkin) == len(tex) == len(Ntot) == len(xoff_v) == len(width) == len(fortho)):
             raise ValueError("Wrong array lengths!")
 
+        modelkwargs = kwargs.copy()
         def L(x):
             v = np.zeros(len(x))
             for i in range(len(tkin)):
-                v += self.ammonia(x,tkin=tkin[i],tex=tex[i],Ntot=Ntot[i],width=width[i],xoff_v=xoff_v[i],fortho=fortho[i],**kwargs)
+                modelkwargs.update({'tkin':tkin[i], 'tex':tex[i],
+                        'width':width[i], 'xoff_v':xoff_v[i],
+                        'fortho':fortho[i]})
+                if fittau:
+                    modelkwargs.update({'tau11':Ntot[i]})
+                else:
+                    modelkwargs.update({'Ntot':Ntot[i]})
+                v += self.ammonia(x,**modelkwargs)
             return v
         return L
 