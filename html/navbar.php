90875586f65154491178ef8ae9fcdca8967d65a8
Adam Ginsburg <keflavich@gmail.com>
2011-03-22 13:45:28 -0600

    added php navbar, though I don't know if php is supported on bitbucket

---
+++ html/navbar.php
@@ -0,0 +1,5 @@
+<?php
+echo "<center><p class=navbar><nobr>[&nbsp;<a class=navbar href=\"../index.html\">Home</a>&nbsp;]</nobr>";
+echo "<nobr>[&nbsp;<a class=navbar href=\"examples.html\">Examples</a>&nbsp;]</nobr>";
+?>
+
