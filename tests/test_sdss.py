7dea854f1d4745a00633a1b41a816a398253c1ea
Jordan Mirocha <mirochaj@gmail.com>
2011-07-26 11:37:54 -0600

    Added sdss spectrum example to html.

--- tests/test_sdss.py
+++ tests/test_sdss.py
@@ -1,6 +1,6 @@
 import pyspeckit
 
-# Rest wavelengths of the lines we are fitting
+# Rest wavelengths of the lines we are fitting - use as initial guesses
 NIIa = 6549.86
 NIIb = 6585.27
 Halpha = 6564.614
@@ -9,12 +9,13 @@ SIIb = 6732.68
 
 # Initialize spectrum object and plot region surrounding Halpha-[NII] complex
 spec = pyspeckit.Spectrum('sample_sdss.txt', errspecnum=2)
-spec.plotter(xmin = 6450, xmax = 6775, ymin = -20, ymax = 120)
+spec.plotter(xmin = 6450, xmax = 6775, ymin = 0, ymax = 150)
 
 # Use [SII] lines to model narrow lines, then force [NII] lines and narrow H-alpha to have same width as [SII].  
-# Will fit 2 additional broad components to H-alpha (standard for AGN spectra)
-guesses = [50, NIIa, 5, 100, Halpha, 5, 50, Halpha, 50, 50, NIIb, 5, 20, SIIa, 5, 20, SIIb, 5, 20, Halpha, 5]
-tied = ['', '', 'p[17]', '', '', 'p[17]', '', 'p[4]', '', '3 * p[0]', '', 'p[17]', '', '', 'p[17]', '','','', '', '', '']
+# Will fit 1 additional broad component to H-alpha (standard for AGN spectra)
+# Wavelengths are all tied together
+guesses = [50, NIIa, 5, 100, Halpha, 5, 50, Halpha, 50, 50, NIIb, 5, 20, SIIa, 5, 20, SIIb, 5]
+tied = ['', '', 'p[17]', '', '', 'p[17]', '', 'p[4]', '', '3 * p[0]', '', 'p[17]', '', '', 'p[17]', '', '', '']
 
 # Actually do the fit.
 spec.specfit(guesses = guesses, tied = tied, annotate = False)
@@ -38,12 +39,15 @@ spec.plotter.axis.set_ylabel(r'Flux $(10^{-17} \mathrm{erg/s/cm^2/\AA})$')
 spec.plotter.refresh()
 
 # Print out spectral line information
-print "Line   Flux (erg/s/cm^2)    FWHM (Angstrom)   Luminosity (erg/s)   Amplitude (erg/s/cm^2)"
+print "Line   Flux (erg/s/cm^2)     Amplitude (erg/s/cm^2)    FWHM (Angstrom)   Luminosity (erg/s)"
 for line in spec.measurements.lines.keys():
-    print line, spec.measurements.lines[line]['flux'], spec.measurements.lines[line]['fwhm'], spec.measurements.lines[line]['lum'], \
-        spec.measurements.lines[line]['amp']
-
+    print line, spec.measurements.lines[line]['flux'], spec.measurements.lines[line]['amp'], spec.measurements.lines[line]['fwhm'], \
+        spec.measurements.lines[line]['lum']
+        
 # Notice that because we supplied the objects redshift and flux normalization, the measurements class
 # automatically calculated line luminosities.  Also, it separates the broad and narrow H-alpha components. How nice!
+
+# Save the figure
+spec.plotter.figure.savefig("sdss_fit_example.png")
 
 raw_input('Done.')
