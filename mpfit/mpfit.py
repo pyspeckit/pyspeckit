be31039591baf9e47b6e362153a239cff33ea579
Adam Ginsburg <keflavich@gmail.com>
2011-10-03 20:30:20 -0600

    removed scipy dependency from mpfit

--- mpfit/mpfit.py
+++ mpfit/mpfit.py
@@ -410,7 +410,6 @@ Perform Levenberg-Marquardt least-squares minimization, based on MINPACK-1.
 
 import numpy
 import types
-import scipy.lib.blas
 
 #	 Original FORTRAN documentation
 #	 **********
@@ -595,9 +594,6 @@ import scipy.lib.blas
 #	 **********
 
 class mpfit:
-
-	blas_enorm32, = scipy.lib.blas.get_blas_funcs(['nrm2'],numpy.array([0],dtype=numpy.float32))
-	blas_enorm64, = scipy.lib.blas.get_blas_funcs(['nrm2'],numpy.array([0],dtype=numpy.float64))
 
 
 	def __init__(self, fcn, xall=None, functkw={}, parinfo=None,
@@ -1004,10 +1000,8 @@ class mpfit:
 		# the returned value, not by the precision of the input array
 		if numpy.array([fvec]).dtype.itemsize>4:
 			self.machar = machar(double=1)
-			self.blas_enorm = mpfit.blas_enorm64
 		else:
 			self.machar = machar(double=0)
-			self.blas_enorm = mpfit.blas_enorm32
 		machep = self.machar.machep
 		
 		m = len(fvec)
@@ -1496,7 +1490,7 @@ class mpfit:
 	
 	
 	def enorm(self, vec):
-		ans = self.blas_enorm(vec)
+		ans = numpy.sqrt(numpy.dot(vec.T, vec))
 		return ans
 	
 	
