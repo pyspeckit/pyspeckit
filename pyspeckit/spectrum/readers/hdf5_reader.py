5ef23171951c0b27518b3dc824dbc153e192c041
Jordan Mirocha <mirochaj@gmail.com>
2011-11-21 11:22:14 -0700

    Minor update to hdf5_reader.py

--- pyspeckit/spectrum/readers/hdf5_reader.py
+++ pyspeckit/spectrum/readers/hdf5_reader.py
@@ -1,3 +1,15 @@
+"""
+==========================
+PySpecKit HDF5 Reader
+==========================
+
+Routines for reading in spectra from HDF5 files.
+
+Note: Current no routines for parsing HDF5 headers in classes.py.
+
+.. moduleauthor:: Adam Ginsburg <adam.g.ginsburg@gmail.com>
+.. moduleauthor:: Jordan Mirocha <mirochaj@gmail.com>
+"""
 try: 
     import h5py
     h5OK = True
@@ -13,28 +25,30 @@ try:
 except ImportError: 
     pyfitscheck = False
 
-def open_hdf5(filename):
+def open_hdf5(filename, xaxkey = 'xarr', datakey = 'data', errkey = 'error'):
     """
-    This reader expects three datasets to exist in the hdf5 file 'filename': 'xarr', 'data', and 'error'.
+    This reader expects three datasets to exist in the hdf5 file 'filename': 
+    'xarr', 'data', and 'error', by default.  Can specify other dataset names.
     """
     
-    if not h5OK: print "Cannot read hdf5 format - h5py import failed."
+    if not h5OK: 
+        print "Cannot read hdf5 format - h5py import failed."
     else:
         f = h5py.File(filename, 'r')
         
-        try: xarr = f['xarr'].value
-        except KeyError: print 'Dataset \'xarr\' not found.'
+        try: xarr = f[xaxkey].value
+        except KeyError: print 'Dataset \'%s\' not found.' % xaxkey
         
-        try: data = f['data'].value
-        except KeyError: print 'Dataset \'data\' not found.'
+        try: data = f[datakey].value
+        except KeyError: print 'Dataset \'%s\' not found.' % datakey
     
-        try: error = f['error'].value
+        try: error = f[errkey].value
         except KeyError: 
-            print 'Dataset \'err\' not found.'
+            print 'Dataset \'%s\' not found.' % errkey
             error = np.ones_like(data)
             
         try: 
-            xunits = f['xarr'].attrs['xunits']
+            xunits = f[xaxkey].attrs['xunits']
             if xunits == '': xunits = 'unknown'
         except KeyError: xunits = 'unknown'    
             
