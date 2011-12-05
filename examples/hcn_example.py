eb7884adf37e4392eb2c74072d345165b1e8de85
Adam Ginsburg <keflavich@gmail.com>
2011-12-05 10:02:56 -0700

    Added some additional examples (probably all incomplete)

---
+++ examples/hcn_example.py
@@ -0,0 +1,22 @@
+import pyspeckit
+
+# Load the spectrum
+sp = pyspeckit.Spectrum('hcn_example.fits')
+
+# Register the fitter
+# The N2H+ fitter is 'built-in' but is not registered by default; this example
+# shows how to register a fitting procedure
+# 'multi' indicates that it is possible to fit multiple components and a background will not automatically be fit
+# 4 is the number of parameters in the model (excitation temperature, optical depth, line center, and line width)
+sp.Registry.add_fitter('hcn_vtau',pyspeckit.models.hcn.hcn_vtau_fitter,4,multisingle='multi')
+
+# Run the fitter
+sp.specfit(fittype='hcn_vtau',multifit=True,guesses=[15,2,4,0.2])
+
+# Plot the results
+sp.plotter()
+# Re-run the fitter (to get proper error bars) and show the individual fit components
+sp.specfit(fittype='hcn_vtau',multifit=True,guesses=[15,2,4,0.2],show_components=True)
+
+# Save the figure (this step is just so that an image can be included on the web page)
+sp.plotter.savefig('hcn_example_fit.png')
---
+++ examples/voigt.py
@@ -0,0 +1,22 @@
+import pyspeckit
+import numpy as np
+from pyspeckit.spectrum.models import voigtfitter
+
+
+xarr = pyspeckit.spectrum.units.SpectroscopicAxis(np.linspace(-100,100,500),unit='km/s',refX=1e9,refX_units='Hz')
+VF = voigtfitter.voigt_fitter()
+
+sp = pyspeckit.Spectrum(xarr=xarr, data=VF.voigt(xarr,1,0,2.5,2.5) + np.random.randn(xarr.shape[0])/20., error=np.ones(xarr.shape[0])/20.)
+sp.plotter()
+sp.specfit(fittype='voigt')
+sp.specfit(fittype='gaussian',composite_fit_color='b',clear=False,annotate=False)
+
+sp = pyspeckit.Spectrum(xarr=xarr, data=VF.voigt(xarr,1,0,2.5,5.0) + np.random.randn(xarr.shape[0])/20., error=np.ones(xarr.shape[0])/20.)
+sp.plotter()
+sp.specfit(fittype='voigt')
+sp.specfit(fittype='gaussian',composite_fit_color='b',clear=False,annotate=False)
+
+sp = pyspeckit.Spectrum(xarr=xarr, data=VF.voigt(xarr,1,0,2.5,5.0) + np.random.randn(xarr.shape[0])/50., error=np.ones(xarr.shape[0])/50.)
+sp.plotter()
+sp.specfit(fittype='voigt')
+sp.specfit(fittype='gaussian',composite_fit_color='b',clear=False,annotate=False)
