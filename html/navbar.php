8f75a07df8ecb6ef725464e15f6a52372b1e7bde
Adam Ginsburg <keflavich@gmail.com>
2011-12-22 08:59:43 -0700

    updated navigation & style

--- html/navbar.php
+++ html/navbar.php
@@ -1,5 +1,6 @@
 <?php
-echo "<center><p class=navbar><nobr>[&nbsp;<a class=navbar href=\"../index.html\">Home</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"examples.html\">Examples</a>&nbsp;]</nobr>";
+echo "<center><p class=navbar><nobr><a class=navbar href=\"../index.html\">Home</a></nobr>";
+echo "<nobr><a class=navbar href=\"examples.html\">Examples</a></nobr>";
+echo "<nobr><a class=navbar href=\"sphinx/index.html\">Documentation</a></nobr>";
 ?>
 
