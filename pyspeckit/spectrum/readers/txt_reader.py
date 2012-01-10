8be1f12660a7525eafbae9740caa64a6ccdeee6c
Adam Ginsburg <keflavich@gmail.com>
2012-01-09 19:35:39 -0700

    minor change - when 'simple' text reader is used, should not warn user that atpy is not available (it may be!)

--- pyspeckit/spectrum/readers/txt_reader.py
+++ pyspeckit/spectrum/readers/txt_reader.py
@@ -36,7 +36,8 @@ def open_1d_txt(filename, xaxcol=0, datacol=1, errorcol=2,
     kwargs are passed to atpy.Table
     """
     if text_reader=='simple' or not atpyOK:
-        warn("WARNING: atpy not installed; will use simple reader instead.")
+        if not atpyOK:
+            warn("WARNING: atpy not installed; will use simple reader instead.")
         
         data, error, XAxis, T = simple_txt(filename, xaxcol = xaxcol, 
             datacol = datacol, errorcol = errorcol, **kwargs)
