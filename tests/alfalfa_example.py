f689a0090fe101c00880cd32f2ce6879c08b36e4
Adam Ginsburg <keflavich@gmail.com>
2012-02-27 22:22:39 -0700

    minor change to alfalfa example

--- tests/alfalfa_example.py
+++ tests/alfalfa_example.py
@@ -8,14 +8,17 @@ HI1459 = alfalfa.read_alfalfa_file('HI145947.9+152515_1500+15d.src')
 # print out the contents of the dictionary
 print HI1459
 
+# make a shortcut for the source
+src = HI1459['145947.9+152515']
+
 # we can plot the whole object, but it has 6 spectra, so we're going to get 6 overlapping spectra
-HI1459['145947.9+152515'].plotter()
+src.plotter()
 
 # it may be more useful to plot each item in the "block"
-HI1459['145947.9+152515'].ploteach()
+src.ploteach()
 
 # or you can just plot an individual one (this will not open a new window; it will overlay on a previous plot)
-HI1459['145947.9+152515'][0].plotter(color='r',clear=False)
+src[0].plotter(color='r',clear=False)
 
 
 
