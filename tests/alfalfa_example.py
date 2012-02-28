34311f5551fd39410920f23f3257a3403808b0ca
Adam Ginsburg <keflavich@gmail.com>
2012-02-27 22:02:17 -0700

    added alfalfa_example

---
+++ tests/alfalfa_example.py
@@ -0,0 +1,21 @@
+
+# Import the ALFALFA reader (deals with the specific structure type assumed for
+# an ALFALFA src file)
+from pyspeckit.spectrum.readers import alfalfa
+
+# Read the full contents of an ALFALFA .src file
+HI1459 = alfalfa.read_alfalfa_file('HI145947.9+152515_1500+15d.src')
+# print out the contents of the dictionary
+print HI1459
+
+# we can plot the whole object, but it has 6 spectra, so we're going to get 6 overlapping spectra
+HI1459['145947.9+152515'].plotter()
+
+# it may be more useful to plot each item in the "block"
+HI1459['145947.9+152515'].ploteach()
+
+# or you can just plot an individual one (this will not open a new window; it will overlay on a previous plot)
+HI1459['145947.9+152515'][0].plotter(color='r',clear=False)
+
+
+
