c4df5edb20bc9fe4efbdda9106d731a3a33524d1
Adam Ginsburg <keflavich@gmail.com>
2012-03-03 18:29:58 -0700

    added the 3c286 test

---
+++ tests/test_3c286.py
@@ -0,0 +1,31 @@
+from pyspeckit.spectrum.readers import gbt
+session = gbt.GBTSession('3C286.fits')
+print session
+session.load_target('3C286')
+target = session['3C286']
+sp = target.blocks['A13OFF2'][0] # there are 4 identical spectra
+sp.xarr.convert_to_unit('km/s',convention='optical')
+sp.plotter(xmin=122610,xmax=122660)
+stats = sp.stats(statrange=(-20+122610,10+122610))
+sp.error[:] = stats['std']
+sp.specfit(fittype='gaussian')
+sp.specfit.plotresiduals(drawstyle='line')
+print "Gaussian chi^2: %g  chi^2/n: %g" % (sp.specfit.chi2, sp.specfit.chi2/sp.specfit.dof)
+sp.specfit(fittype='voigt', clear=False, composite_fit_color='blue')
+sp.specfit.plotresiduals(clear=False, color='blue', drawstyle='line')
+print "Voigt   chi^2: %g  chi^2/n: %g" % (sp.specfit.chi2, sp.specfit.chi2/sp.specfit.dof)
+sp.specfit.residualaxis.set_ylim(-0.2,0.2)
+
+# this is usually unnecessary, but it's platform-dependent
+import pylab
+pylab.ion()
+pylab.show()
+
+
+import pyspeckit
+sp1 = target.blocks['A13OFF2'][0]
+sp2 = target.blocks['A13OFF2'][1]
+xc = pyspeckit.correlate.correlate(sp1,sp2,range=[122500,123000],units='km/s')
+xc.plotter()
+xc.specfit()
+
