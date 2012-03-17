c90ccdf08a6c91d4619244f87ba75f753623f07f
Adam Ginsburg <keflavich@gmail.com>
2012-03-17 10:43:59 -0600

    error propagation in interpolation & correlation (sort of)

--- pyspeckit/spectrum/correlate.py
+++ pyspeckit/spectrum/correlate.py
@@ -5,7 +5,7 @@ import classes
 import pyfits
 import headers
 
-def correlate(spectrum1, spectrum2, range=None, units=None):
+def correlate(spectrum1, spectrum2, range=None, units=None, errorweight=False):
     """
     Cross-correlate spectrum1 with spectrum2
 
@@ -22,6 +22,17 @@ def correlate(spectrum1, spectrum2, range=None, units=None):
     data2 = spectrum2.data
 
     xcorr = np.correlate(data1, data2, mode='same')
+
+    # very simple propagation of error
+    # each element is multiplied, multiplicative error is given such that (sigma_xy/xy)**2 = (sigma_x/x)**2 + (sigma_y/y)**2
+    # error = (np.correlate( (spectrum1.error/spectrum1.data)**2 , np.ones(xcorr.shape), mode='same') + 
+    #          np.correlate( (spectrum2.error/spectrum2.data)**2 , np.ones(xcorr.shape), mode='same'))**0.5 * xcorr
+    # That approach sucks - what if data == 0?
+    #
+    # this might be more correct: http://arxiv.org/pdf/1006.4069v1.pdf eqn 4
+    # but it doesn't quite fit my naive expectations so:
+    error = (np.correlate( (spectrum1.error)**2 , np.ones(xcorr.shape), mode='same') + 
+             np.correlate( (spectrum2.error)**2 , np.ones(xcorr.shape), mode='same'))**0.5
     
     xarr = spectrum1.xarr
     xrange = xarr.max()-xarr.min()
@@ -39,5 +50,5 @@ def correlate(spectrum1, spectrum2, range=None, units=None):
         header.update('CRVAL1',xmin.tolist())
     header.update('CDELT1',offset_xarr.cdelt())
 
-    return classes.XCorrSpectrum(xarr=offset_xarr, data=xcorr, header=header)
+    return classes.XCorrSpectrum(xarr=offset_xarr, data=xcorr, header=header, error=error)
 
