Awesome new tools in units: frame conversion!  Not fully integrated, but lots of tools are now available! Fixed errspec = error instead of errspec=error.copy() in fitters (thanks Jordan) (danger - can this still be installed without slalib?)

pyspeckit/spectrum/fitters.py
@@ -281,7 +281,7 @@ class Specfit(interactive.Interactive):
                 else:
                     self.errspec = self.Spectrum.error + 1
             else:
-                self.errspec = self.Spectrum.error
+                self.errspec = self.Spectrum.error.copy()
         else: self.errspec = np.ones(self.spectofit.shape[0]) * self.spectofit.std()
 
     def setfitspec(self):