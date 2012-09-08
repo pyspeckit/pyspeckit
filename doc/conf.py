ec6d45a72c14e5ec43a3e49f032bb9ca96149d34
Adam Ginsburg <keflavich@gmail.com>
2012-09-08 12:55:28 -0600

    updated REQUIREMENTS and conf for readthedocs compatibility

--- doc/conf.py
+++ doc/conf.py
@@ -242,3 +242,28 @@ man_pages = [
     ('index', 'pyspeckit', u'pyspeckit Documentation',
      [u'Adam Ginsburg and Jordan Mirocha'], 1)
 ]
+
+
+# read the docs mocks
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
