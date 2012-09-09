acba908eae7ebc03d717e7edf7da77368272eca5
Adam Ginsburg <keflavich@gmail.com>
2012-09-08 22:04:18 -0600

    removed numpy from requirements - only depends on numpydoc now the rest get mocked

--- doc/conf.py
+++ doc/conf.py
@@ -268,7 +268,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy','matplotlib']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
