d28bb366f2ec34c0bfb9db67536d5f746a0556ac
Adam Ginsburg <keflavich@gmail.com>
2012-09-10 22:42:54 -0600

    conf update attempts for rtd

--- doc/conf.py
+++ doc/conf.py
@@ -272,7 +272,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy','matplotlib','pyfits']
+MOCK_MODULES = ['matplotlib','pyfits','scipy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
