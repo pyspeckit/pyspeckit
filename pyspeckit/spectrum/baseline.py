fef9b94fc8f0c86c9976829f55b24cd31352c7c0
Adam Ginsburg <keflavich@gmail.com>
2012-10-16 23:46:31 -0600

    allow powerlaw override in baseline fitting

--- pyspeckit/spectrum/baseline.py
+++ pyspeckit/spectrum/baseline.py
@@ -150,7 +150,7 @@ class Baseline(interactive.Interactive):
             if highlight: self.highlight_fitregion()
         if save: self.savefit()
 
-    def button2action(self, event=None, debug=False, subtract=True,
+    def button2action(self, event=None, debug=False, subtract=True, powerlaw=None,
             fit_original=False, baseline_fit_color='orange', **kwargs):
         """
         Do the baseline fitting and save and plot the results.
@@ -159,6 +159,9 @@ class Baseline(interactive.Interactive):
         if debug: print "Button 2/3 Baseline.  Subtract=",subtract
         if self.subtracted:
             self.unsubtract()
+
+        if powerlaw is not None:
+            powerlaw = self.powerlaw
 
         self.clear_highlights()
 
@@ -171,7 +174,7 @@ class Baseline(interactive.Interactive):
                 err=self.Spectrum.error,
                 order=self.order, 
                 mask=(True-self.includemask),
-                powerlaw=self.powerlaw,
+                powerlaw=powerlaw,
                 xarr_fit_units=xarr_fit_units,
                 **kwargs)
 
