9623d09ff7cbf1b7f6e1c978a118cb6d32c72994
Adam Ginsburg <keflavich@gmail.com>
2013-03-28 23:39:53 -0600

    completed the super-simple untested spectral template model script

--- pyspeckit/spectrum/models/template.py
+++ pyspeckit/spectrum/models/template.py
@@ -6,6 +6,9 @@ def spectral_template(xarr, template_spectrum, scale, xshift, xshift_units='km/s
     Given a template Spectrum (which should be a Spectrum instance),
     scale & shift it
     """
-    model = _interp(xarr, template_spectrum.xarr, template_spectrum.data, left=np.nan, right=np.nan)
-    pass
+    shift = xarr.x_to_units(xshift, xshift_units)
+    model = _interp(xarr, template_spectrum.xarr.as_unit(xarr.units)+shift,
+            template_spectrum.data, left=np.nan, right=np.nan) * scale
+    
+    return model
 
