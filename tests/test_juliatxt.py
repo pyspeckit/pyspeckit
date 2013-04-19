077c7759277cff6aea885f858305fe372e5c4e3a
Adam Ginsburg <keflavich@gmail.com>
2013-04-19 15:23:42 -0600

    updated test_julia

--- tests/test_juliatxt.py
+++ tests/test_juliatxt.py
@@ -1,2 +1,10 @@
 import pyspeckit
 sp = pyspeckit.Spectrum('julia_infrared.txt')
+
+# set up units properly
+sp.xarr.units='angstroms'
+sp.xarr.xtype = 'wavelength'
+sp.units = r'erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$'
+
+# plot!
+sp.plotter()
