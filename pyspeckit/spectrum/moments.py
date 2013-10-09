3364a2b88b0f777c86c73b5fdad7b3134fd274c8
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-10-09 12:39:46 -0400

    refactored moments docs

--- pyspeckit/spectrum/moments.py
+++ pyspeckit/spectrum/moments.py
@@ -1,27 +1,17 @@
 import numpy as np
 
 def moments(Xax, data, vheight=True, estimator=np.mean, negamp=None,
-        veryverbose=False, nsigcut=None, noise_guess=None,  **kwargs):
-    """Returns (height, amplitude, x, width_x)
-    the gaussian parameters of a 1D distribution by calculating its
-    moments.  Depending on the input parameters, will only output
-    a subset of the above.
-
-    If using masked arrays, pass estimator=np.ma.median
-    'estimator' is used to measure the background level (height)
-
-    negamp can be used to force the peak negative (True), positive (False),
-    or it will be "autodetected" (negamp=None)
-
-    "nsigcut" - try to estimate the noise and only use data above/below nsigma
-    above the noise.  Estimate the noise from the data unless passed as a
-    keyword
+            veryverbose=False, nsigcut=None, noise_estimate=None,  **kwargs):
+    """
+    Returns the gaussian parameters of a 1D distribution by calculating its
+    moments.  Depending on the input parameters, will only output a subset of
+    the above.
 
     Theory, from first principles (in the absence of noise):
     integral(gaussian) = sqrt(2*pi*sigma^2) * amp
     sigma = integral / amp / sqrt(2*pi)
     
-    in the presence of noise, this gets much more complicated
+    In the presence of noise, this gets much more complicated.
 
     Parameters
     ----------
@@ -33,6 +23,23 @@ def moments(Xax, data, vheight=True, estimator=np.mean, negamp=None,
         is the first moment
     width_x:
         is the second moment
+    estimator: function
+        A function to estimate the "height" or "background level" of the data,
+        e.g. mean or median.  If masked arrays are being used, use the np.ma
+        versions of the numpy functions
+    negamp: bool or None
+        Force the peak negative (True), positive (False), or the sign of the
+        peak will be "autodetected" (negamp=None)
+    nsigcut: float or None
+        If specified, the code will attempt to estimate the noise and only use
+        data above/below `n`-sigma above the noise.  The noise will be estimated
+        from the data unless the noise is specified with `noise_estimate`
+    noise_estimate: float or None
+        Guess for the noise value.  Only matters if `nsigcut` is specified.
+
+    Returns
+    -------
+    (height, amplitude, x, width_x)
     """
 
     Xax = np.array(Xax)
@@ -49,10 +56,10 @@ def moments(Xax, data, vheight=True, estimator=np.mean, negamp=None,
     integral = (data*dx).sum()
     height = estimator(data)
 
-    if noise_guess is None:
-        noise_guess = data.std() 
-    height_cut_low = height-nsigcut*noise_guess if nsigcut is not None else height
-    height_cut_high = height+nsigcut*noise_guess if nsigcut is not None else height
+    if noise_estimate is None:
+        noise_estimate = data.std()
+    height_cut_low = height-nsigcut*noise_estimate if nsigcut is not None else height
+    height_cut_high = height+nsigcut*noise_estimate if nsigcut is not None else height
     
     # try to figure out whether pos or neg based on the minimum width of the pos/neg peaks
     data_gt_low = data>height_cut_low
@@ -86,12 +93,12 @@ def moments(Xax, data, vheight=True, estimator=np.mean, negamp=None,
 
     if veryverbose:
         print "Hstddev: %g   Lstddev: %g" % (Hstddev,Lstddev)
-        print "negamp: %s  amp,width,cen Lower: %g, %g   Upper: %g, %g  Center: %g" %\
-                (negamp,Lamplitude,Lwidth_x,Hamplitude,Hwidth_x,xcen)
+        print ("negamp: %s  amp,width,cen Lower: %g, %g   Upper: %g, %g  Center: %g" %
+               (negamp,Lamplitude,Lwidth_x,Hamplitude,Hwidth_x,xcen))
     mylist = [amplitude,xcen,width_x]
-    if negamp and amplitude > 0 and veryverbose: 
+    if negamp and amplitude > 0 and veryverbose:
         print "WARNING: likely fit failure.  negamp=True, but amplitude > 0"
-    if negamp is False and amplitude < 0 and veryverbose: 
+    if negamp is False and amplitude < 0 and veryverbose:
         print "WARNING: likely fit failure.  negamp=False, but amplitude < 0"
     if np.isnan(width_x) or np.isnan(height) or np.isnan(amplitude):
         raise ValueError("something is nan")
