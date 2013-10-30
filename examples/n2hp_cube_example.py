fe5b324fbedbe1fbb0a146edb0886dbe572bbb20
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-10-30 21:21:01 +0100

    fixed N2HP cube example; it's pretty buggy though

--- examples/n2hp_cube_example.py
+++ examples/n2hp_cube_example.py
@@ -1,7 +1,18 @@
 import pyspeckit
+import os
+
+if not os.path.exists('n2hp_cube.fit'):
+    import astropy.utils.data as aud
+    from astropy.io import fits
+    f = aud.download_file('ftp://cdsarc.u-strasbg.fr/pub/cats/J/A%2BA/472/519/fits/opha_n2h.fit')
+    with fits.open(f) as ff:
+        ff[0].header['CUNIT3'] = 'm/s'
+        for kw in ['CTYPE4','CRVAL4','CDELT4','CRPIX4']:
+            del ff[0].header[kw]
+        ff.writeto('n2hp_cube.fit')
 
 # Load the spectral cube
-spc = pyspeckit.Cube('n2hp_cube.fits')
+spc = pyspeckit.Cube('n2hp_cube.fit')
 
 # Register the fitter
 # The N2H+ fitter is 'built-in' but is not registered by default; this example
@@ -13,8 +24,9 @@ spc.Registry.add_fitter('n2hp_vtau',pyspeckit.models.n2hp.n2hp_vtau_fitter,4,mul
 
 # Run the fitter
 spc.fiteach(fittype='n2hp_vtau', multifit=True,
-        guesses=[5,0.5,3,1], # Tex=5K, tau=0.5, v_center=12, width=1 km/s
-        )
+            guesses=[5,0.5,3,1], # Tex=5K, tau=0.5, v_center=12, width=1 km/s
+            signal_cut=6, # minimize the # of pixels fit for the example
+            )
 # There are a huge number of parameters for the fiteach procedure.  See:
 # http://pyspeckit.readthedocs.org/en/latest/example_nh3_cube.html
 # http://pyspeckit.readthedocs.org/en/latest/cubes.html?highlight=fiteach#pyspeckit.cubes.SpectralCube.Cube.fiteach
@@ -33,7 +45,7 @@ spc.mapplot()
 # spectrum & fit
 
 # plot one of the fitted spectra
-spc.plot_spectrum(0,0,plot_fit=True)
+spc.plot_spectrum(14,27,plot_fit=True)
 
 # Show an image of the best-fit velocity
 spc.mapplot.plane = spc.parcube[2,:,:]
