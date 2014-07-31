e18bd4d4a165443b90b30f50c2848da0df7369ed
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-31 10:34:50 +0200

    remove packagename junk

--- pyspeckit/__init__.py
+++ pyspeckit/__init__.py
@@ -2,21 +2,24 @@
 :Author: Adam Ginsburg <adam.g.ginsburg@gmail.com> and Jordan Mirocha <mirochaj@gmail.com>
 .. moduleauthor:: Adam Ginsburg <adam.g.ginsburg@gmail.com>
 """
-__all__ = ['spectrum','cubes','wrappers']
-from version import version as __version__
-import spectrum
-import specwarnings
-try:
-    import cubes
-except ImportError:
-    specwarnings.warn( "pyspeckit.cubes module not imported - cubes requires pywcs" )
-import wrappers
-from wrappers import *
-from cubes import *
-from spectrum import *
+from ._astropy_init import *
 
-def test(*args, **kwargs):
-    import tests
-    #import os
-    #os.chdir(os.path.split(os.path.abspath(tests.__file__))[0])
-    tests.run_tests.test_everything()
+__all__ = ['spectrum','cubes','wrappers']
+if not _ASTROPY_SETUP_:
+    from version import version as __version__
+    import spectrum
+    import specwarnings
+    try:
+        import cubes
+    except ImportError:
+        specwarnings.warn( "pyspeckit.cubes module not imported - cubes requires pywcs" )
+    import wrappers
+    from wrappers import *
+    from cubes import *
+    from spectrum import *
+
+    def test(*args, **kwargs):
+        import tests
+        #import os
+        #os.chdir(os.path.split(os.path.abspath(tests.__file__))[0])
+        tests.run_tests.test_everything()
