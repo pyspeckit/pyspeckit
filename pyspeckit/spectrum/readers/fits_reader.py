0fea442cb7f0cb1bec2ffc61c20e9d89bca474d1
Miguel de Val-Borro <miguel@archlinux.net>
2014-12-06 18:59:15 -0500

    Replace div operator by truediv

--- pyspeckit/spectrum/readers/fits_reader.py
+++ pyspeckit/spectrum/readers/fits_reader.py
@@ -7,6 +7,7 @@ import numpy.ma as ma
 import numpy as np
 from . import make_axis
 import operator
+from __future__ import division
 from pyspeckit.specwarnings import warn
 
 def open_1d_fits(filename, hdu=0, **kwargs):
@@ -37,7 +38,7 @@ def open_1d_fits(filename, hdu=0, **kwargs):
 
 def open_1d_pyfits(pyfits_hdu, specnum=0, wcstype='', specaxis="1",
         errspecnum=None, autofix=True, scale_keyword=None,
-        scale_action=operator.div, verbose=False, apnum=0, **kwargs):
+        scale_action=operator.truediv, verbose=False, apnum=0, **kwargs):
     """
     This is open_1d_fits but for a pyfits_hdu so you don't necessarily have to
     open a fits file
