9b3ec024d5b579b7d684a0062577a6474732c146
Dinos Kousidis <dinos.kousidis@tum.de>
2015-04-16 10:04:20 +0200

    removed .to_string() from SpectralCube.py

--- pyspeckit/cubes/SpectralCube.py
+++ pyspeckit/cubes/SpectralCube.py
@@ -105,7 +105,7 @@ class Cube(spectrum.Spectrum):
                 log.debug("Self.unit: {0}".format(self.unit))
                 if xarr is None:
                     xarr = SpectroscopicAxis(cube.spectral_axis,
-                                             unit=cube.spectral_axis.unit.to_string(), #TODO: accept astropy unit
+                                             unit=cube.spectral_axis.unit,
                                              refX=cube.wcs.wcs.restfrq, refX_unit='Hz')
                 if header is None:
                     header = cube.header
