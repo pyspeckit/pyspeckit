11a7bcb7580db93c40c64a8730966b129f84c35a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-30 18:01:57 +0200

    add a new ammonia example

---
+++ examples/ammonia_vtau_multitem_example.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pyspeckit
+from astropy import units as u
+from pyspeckit.spectrum.models import ammonia_constants, ammonia
+
+# Generate a synthetic spectrum based off of 3 NH3 lines
+xarr11 = pyspeckit.units.SpectroscopicAxis(np.linspace(-30, 30, 100)*u.km/u.s,
+                                           velocity_convention='radio',
+                                           refX=ammonia_constants.freq_dict['oneone']).as_unit(u.GHz)
+xarr22 = pyspeckit.units.SpectroscopicAxis(np.linspace(-30, 30, 100)*u.km/u.s,
+                                           velocity_convention='radio',
+                                           refX=ammonia_constants.freq_dict['twotwo']).as_unit(u.GHz)
+xarr33 = pyspeckit.units.SpectroscopicAxis(np.linspace(-30, 30, 100)*u.km/u.s,
+                                           velocity_convention='radio',
+                                           refX=ammonia_constants.freq_dict['threethree']).as_unit(u.GHz)
+xarr = pyspeckit.units.SpectroscopicAxes([xarr11,xarr22,xarr33])
+
+synthspec = (ammonia.ammonia(xarr, tkin=20, ntot=15, fortho=0.5) +
+             ammonia.ammonia(xarr, tkin=50, ntot=14, fortho=0.5))
+
+spectrum = pyspeckit.Spectrum(xarr=xarr, data=synthspec)
+
+spectrum.plotter()
