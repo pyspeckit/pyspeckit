2c226339b29faf3cc1ba7bf969791afbf8882b4b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-30 20:30:55 +0200

    WIP!!!

--- examples/ammonia_vtau_multitem_example.py
+++ examples/ammonia_vtau_multitem_example.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pyspeckit
 from astropy import units as u
-from pyspeckit.spectrum.models import ammonia_constants, ammonia
+from pyspeckit.spectrum.models import ammonia_constants, ammonia, ammonia_hf
 
 # Generate a synthetic spectrum based off of 3 NH3 lines
 xarr11 = pyspeckit.units.SpectroscopicAxis(np.linspace(-30, 30, 100)*u.km/u.s,
@@ -21,3 +21,7 @@ synthspec = (ammonia.ammonia(xarr, tkin=20, ntot=15, fortho=0.5) +
 spectrum = pyspeckit.Spectrum(xarr=xarr, data=synthspec)
 
 spectrum.plotter()
+
+spectrum.specfit.Registry.add_fitter('nh3_vtau', ammonia_hf.nh3_vtau.fitter, ammonia_hf.nh3_vtau.fitter.npars)
+# parnames=['Tex','tau','center','width'],
+spectrum.specfit(fittype='nh3_vtau', guesses=[5, 0.5, 0.5, 0.5])
--- pyspeckit/spectrum/models/ammonia_hf.py
+++ pyspeckit/spectrum/models/ammonia_hf.py
@@ -17,26 +17,42 @@ from pyspeckit.mpfit import mpfit
 import fitter
 import matplotlib.cbook as mpcb
 import copy
+import collections
 
+from . import hyperfine
+from . import radex_modelgrid
 from ammonia_constants import (line_names, freq_dict, aval_dict, ortho_dict,
                                voff_lines_dict, tau_wts_dict)
 
+relative_strength_total_degeneracy = collections.defaultdict(lambda: 1)
 
-nh3_vtau = hyperfine.hyperfinemodel(line_names, voff_lines_dict, freq_dict,
-                                    line_strength_dict,
-                                    relative_strength_total_degeneracy)
-nh3_vtau_fitter = nh3_vtau.fitter
-nh3_vtau_vheight_fitter = nh3_vtau.vheight_fitter
+nh3_vtau = {linename:
+            hyperfine.hyperfinemodel({lineid:name for lineid,name in
+                                      enumerate(line_names[linename])},
+                                     {lineid:voff for lineid,voff in
+                                      enumerate(voff_lines_dict[linename])},
+                                     {lineid:freq for lineid,freq in
+                                      enumerate(freq_dict[linename])},
+                                     {lineid:tauwt for lineid,tauwt in
+                                      enumerate(tau_wts_dict[linename])},
+                                     {lineid:1 for lineid,freq in
+                                      enumerate(freq_dict[linename])},
+                                    )
+            for linename in line_names}
 
-nh3_radex = radex_modelgrid(xarr, density=4, column=13, xoff_v=0.0, width=1.0, 
-        grid_vwidth=1.0,
-        grid_vwidth_scale=False,
-        texgrid=None,
-        taugrid=None,
-        hdr=None,
-        path_to_texgrid='',
-        path_to_taugrid='',
-        temperature_gridnumber=3,
-        modelfunc=nh3_vtau,
-        debug=False,
-        verbose=False)
+def nh3_vtau_multimodel_generator(linenames):
+    def nh3_vtau_multimodel(xarr, velocity, width, *args):
+        assert len(args) == len(linenames)*2
+        models = [nh3_vtau[linename](xarr, tex, tau, velocity, width)
+
+# nh3_vtau_fitter = nh3_vtau.fitter
+# nh3_vtau_vheight_fitter = nh3_vtau.vheight_fitter
+
+#nh3_radex = radex_modelgrid.radex_model(xarr, density=4, column=13, xoff_v=0.0,
+#                                        width=1.0, grid_vwidth=1.0,
+#                                        grid_vwidth_scale=False, texgrid=None,
+#                                        taugrid=None, hdr=None,
+#                                        path_to_texgrid='', path_to_taugrid='',
+#                                        temperature_gridnumber=3,
+#                                        modelfunc=nh3_vtau, debug=False,
+#                                        verbose=False)
