4130b641f70b5f146a0aadc795f0aef0ba2ae814
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-05-02 10:35:45 +0200

    whitespace

--- examples/ammonia_vtau_multitem_example.py
+++ examples/ammonia_vtau_multitem_example.py
@@ -2,26 +2,31 @@ import numpy as np
 import pyspeckit
 from astropy import units as u
 from pyspeckit.spectrum.models import ammonia_constants, ammonia, ammonia_hf
+from pyspeckit.spectrum.models.ammonia_constants import freq_dict
+from pyspeckit.units import SpectroscopicAxis, SpectroscopicAxes
 
-# Step 1. Generate a synthetic spectrum.  Already have a real spectrum?  Skip to step 2!
+# Step 1. Generate a synthetic spectrum.  Already have a real spectrum?  Skip
+# to step 2!
 # Generate a synthetic spectrum based off of 3 NH3 lines
 # Note that they are converted to GHz first
-xarr11 = pyspeckit.units.SpectroscopicAxis(np.linspace(-30, 30, 100)*u.km/u.s,
-                                           velocity_convention='radio',
-                                           refX=ammonia_constants.freq_dict['oneone']).as_unit(u.GHz)
-xarr22 = pyspeckit.units.SpectroscopicAxis(np.linspace(-40, 40, 100)*u.km/u.s,
-                                           velocity_convention='radio',
-                                           refX=ammonia_constants.freq_dict['twotwo']).as_unit(u.GHz)
-xarr33 = pyspeckit.units.SpectroscopicAxis(np.linspace(-50, 50, 100)*u.km/u.s,
-                                           velocity_convention='radio',
-                                           refX=ammonia_constants.freq_dict['threethree']).as_unit(u.GHz)
+xarr11 = SpectroscopicAxis(np.linspace(-30, 30, 100)*u.km/u.s,
+                           velocity_convention='radio',
+                           refX=freq_dict['oneone']).as_unit(u.GHz)
+xarr22 = SpectroscopicAxis(np.linspace(-40, 40, 100)*u.km/u.s,
+                           velocity_convention='radio',
+                           refX=freq_dict['twotwo']).as_unit(u.GHz)
+xarr33 = SpectroscopicAxis(np.linspace(-50, 50, 100)*u.km/u.s,
+                           velocity_convention='radio',
+                           refX=freq_dict['threethree']).as_unit(u.GHz)
 # Merge the three X-axes into a single axis
-xarr = pyspeckit.units.SpectroscopicAxes([xarr11,xarr22,xarr33])
+xarr = SpectroscopicAxes([xarr11,xarr22,xarr33])
 
 # Compute a synthetic model that is made of two temperature components with
 # identical velocities
-synthspec = (ammonia.ammonia(xarr, tkin=20, ntot=15, fortho=0.5, xoff_v=0.0, width=1.0) +
-             ammonia.ammonia(xarr, tkin=50, ntot=14, fortho=0.5, xoff_v=0.0, width=1.0))
+synthspec = (ammonia.ammonia(xarr, tkin=20, ntot=15, fortho=0.5, xoff_v=0.0,
+                             width=1.0) +
+             ammonia.ammonia(xarr, tkin=50, ntot=14, fortho=0.5, xoff_v=0.0,
+                             width=1.0))
 
 # Create the Spectrum object
 spectrum = pyspeckit.Spectrum(xarr=xarr, data=synthspec)
@@ -34,7 +39,8 @@ spectrum.plotter()
 # Use the multi-tex/multi-tau model generator to build up a model function
 # You can use any set of oneone, twotwo, ..., eighteight (no 9-9 or higher)
 # This sets the number of parameters to be fit: 2+2*(n_transitions)
-fitter = ammonia_hf.nh3_vtau_multimodel_generator(['oneone', 'twotwo', 'threethree'])
+fitter = ammonia_hf.nh3_vtau_multimodel_generator(['oneone', 'twotwo',
+                                                   'threethree'])
 
 # Register the fitter - i.e., tell pyspeckit where it is and how to use it
 spectrum.specfit.Registry.add_fitter('nh3_vtau_123', fitter, fitter.npars)
@@ -46,22 +52,39 @@ spectrum.specfit.Registry.add_fitter('nh3_vtau_123', fitter, fitter.npars)
 guesses = [-5, 1.2, 15, 0.5, 15, 0.5, 15, 0.5,]
 
 # Plot up the guessed model
-spectrum.plotter.axis.plot(spectrum.xarr, fitter.n_modelfunc(guesses)(spectrum.xarr), 'b')
+spectrum.plotter.axis.plot(spectrum.xarr,
+                           fitter.n_modelfunc(guesses)(spectrum.xarr), 'b')
 
 # Run the fit!
 spectrum.specfit(fittype='nh3_vtau_123', guesses=guesses)
 
 # display the correct and fitted answers
 print "Low column version:"
-print "Real optical depths of component 1: ",[ammonia.ammonia(xarr, tkin=20, ntot=15, fortho=0.5, xoff_v=0.0, width=1.0, return_tau=True)[x] for x in ['oneone', 'twotwo', 'threethree']]
-print "Real optical depths of component 2: ",[ammonia.ammonia(xarr, tkin=50, ntot=14, fortho=0.5, xoff_v=0.0, width=1.0, return_tau=True)[x] for x in ['oneone', 'twotwo', 'threethree']]
+print "Real optical depths of component 1: ",[ammonia.ammonia(xarr, tkin=20,
+                                                              ntot=15,
+                                                              fortho=0.5,
+                                                              xoff_v=0.0,
+                                                              width=1.0,
+                                                              return_tau=True)[x]
+                                              for x in ['oneone', 'twotwo',
+                                                        'threethree']]
+print "Real optical depths of component 2: ",[ammonia.ammonia(xarr, tkin=50,
+                                                              ntot=14,
+                                                              fortho=0.5,
+                                                              xoff_v=0.0,
+                                                              width=1.0,
+                                                              return_tau=True)[x]
+                                              for x in ['oneone', 'twotwo',
+                                                        'threethree']]
 print "Fitted parameters: ",spectrum.specfit.parinfo
 
 # It works, but the covariances between tex and tau are large.
 # So, another example with higher tau (and therefore... less degenerate?)
 
-synthspec = (ammonia.ammonia(xarr, tkin=20, ntot=16, fortho=0.5, xoff_v=0.0, width=1.0) +
-             ammonia.ammonia(xarr, tkin=50, ntot=15, fortho=0.5, xoff_v=0.0, width=1.0))
+synthspec = (ammonia.ammonia(xarr, tkin=20, ntot=16, fortho=0.5, xoff_v=0.0,
+                             width=1.0) +
+             ammonia.ammonia(xarr, tkin=50, ntot=15, fortho=0.5, xoff_v=0.0,
+                             width=1.0))
 spectrum2 = pyspeckit.Spectrum(xarr=xarr, data=synthspec)
 spectrum2.plotter()
 spectrum2.specfit.Registry.add_fitter('nh3_vtau_123', fitter, fitter.npars)
@@ -69,6 +92,20 @@ spectrum2.specfit(fittype='nh3_vtau_123', guesses=guesses)
 
 # We can also examine what tau really should have been... kinda.
 print "High column version:"
-print "Real optical depths of component 1: ",[ammonia.ammonia(xarr, tkin=20, ntot=16, fortho=0.5, xoff_v=0.0, width=1.0, return_tau=True)[x] for x in ['oneone', 'twotwo', 'threethree']]
-print "Real optical depths of component 2: ",[ammonia.ammonia(xarr, tkin=50, ntot=15, fortho=0.5, xoff_v=0.0, width=1.0, return_tau=True)[x] for x in ['oneone', 'twotwo', 'threethree']]
+print "Real optical depths of component 1: ",[ammonia.ammonia(xarr, tkin=20,
+                                                              ntot=16,
+                                                              fortho=0.5,
+                                                              xoff_v=0.0,
+                                                              width=1.0,
+                                                              return_tau=True)[x]
+                                              for x in ['oneone', 'twotwo',
+                                                        'threethree']]
+print "Real optical depths of component 2: ",[ammonia.ammonia(xarr, tkin=50,
+                                                              ntot=15,
+                                                              fortho=0.5,
+                                                              xoff_v=0.0,
+                                                              width=1.0,
+                                                              return_tau=True)[x]
+                                              for x in ['oneone', 'twotwo',
+                                                        'threethree']]
 print "Fitted parameters: ",spectrum2.specfit.parinfo
