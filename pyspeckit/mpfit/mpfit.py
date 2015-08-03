pyspeckit/pyspeckit

commit 241a2d21839f3cdfa4ca3322d909b9bedbf626cb

bugfix: if vheight has previously been set, it needs to be re-set to False whenever multifitter is called

pyspeckit/spectrum/fitters.py
@@ -516,6 +516,12 @@ class Specfit(interactive.Interactive):
         self.npeaks = len(guesses)/self.Registry.npars[self.fittype]
         self.fitter = self.Registry.multifitters[self.fittype]
         self.vheight = False
+        if self.fitter.vheight:
+            # Need to reset the parinfo if vheight has previously been set,
+            # otherwise npars will disagree, which causes problems if
+            # renormalization happens
+            self.fitter.vheight = False
+            self.fitter._make_parinfo()
 
         # add kwargs to fitkwargs
         self.fitkwargs.update(kwargs)

commit f5d7a5f4290c939ab9f7db68f05f3507c0c2b00f

major bugfix: 'npeaks' was being ignored, so multi-peak fitting would always result in a single fitted peak.  This bug was introduced in https://github.com/pyspeckit/pyspeckit/commit/241a2d21839f3cdfa4ca3322d909b9bedbf626cb to address problems with NULP error checking
Also, lots of whitespace / log modifications.  log.debug will be
verbose, but log.setLevel(5) will show all the mpfit calls.

pyspeckit/mpfit/mpfit.py
@@ -1001,7 +1001,7 @@ class mpfit:
                 return
             self.errmsg = ''
 
-        log.debug("First call to function with parameters {0} and keywords {1}"
+        log.log(10, "First call to function with parameters {0} and keywords {1}"
                   .format(self.params, functkw))
         [self.status, fvec] = self.call(fcn, self.params, functkw)
 
@@ -1071,7 +1071,7 @@ class mpfit:
             # Calculate the jacobian matrix
             self.status = 2
             catch_msg = 'calling MPFIT_FDJAC2'
-            log.debug(catch_msg+" step={0}".format(step))
+            log.log(5, catch_msg+" step={0}".format(step))
             fjac = self.fdjac2(fcn, free_pars_x, fvec, step, qulim, ulim, dside,
                           epsfcn=epsfcn,
                           autoderivative=autoderivative, dstep=dstep,
@@ -1079,7 +1079,7 @@ class mpfit:
             if fjac is None:
                 self.errmsg = 'WARNING: premature termination by FDJAC2'
                 return
-            log.debug("fjac max: {0}".format(fjac.max()))
+            log.log(5, "fjac max: {0}".format(fjac.max()))
             #if numpy.all(fjac == 0):
             #    raise ValueError("All of fjac = 0, so there is no"
             #                     " gradient with parameters.  This probably "
@@ -1094,14 +1094,14 @@ class mpfit:
                 nupeg = len(whupeg)
                 # See if any "pegged" values should keep their derivatives
                 if nlpeg > 0:
-                    log.debug("There are {0} low-pegged parameters".format(nlpeg))
+                    log.log(5, "There are {0} low-pegged parameters".format(nlpeg))
                     # Total derivative of sum wrt lower pegged parameters
                     for i in range(nlpeg):
                         sum0 = sum(fvec * fjac[:,whlpeg[i]])
                         if sum0 > 0:
                             fjac[:,whlpeg[i]] = 0
                 if nupeg > 0:
-                    log.debug("There are {0} high-pegged parameters".format(nupeg))
+                    log.log(5, "There are {0} high-pegged parameters".format(nupeg))
                     # Total derivative of sum wrt upper pegged parameters
                     for i in range(nupeg):
                         sum0 = sum(fvec * fjac[:,whupeg[i]])
@@ -1115,8 +1115,8 @@ class mpfit:
             #                     " gradient with parameters.  This probably "
             #                     "should not happen.")
 
-            log.debug("outer loop; wa1={0}".format(wa1))
-            log.debug("outer loop; wa2={0}".format(wa2))
+            log.log(5, "outer loop; wa1={0}".format(wa1))
+            log.log(5, "outer loop; wa2={0}".format(wa2))
 
             # On the first iteration if "diag" is unspecified, scale
             # according to the norms of the columns of the initial jacobian
@@ -1164,7 +1164,7 @@ class mpfit:
 
             # Compute the norm of the scaled gradient
             catch_msg = 'computing the scaled gradient'
-            log.debug(catch_msg+" fnorm={0}".format(self.fnorm))
+            log.log(5, catch_msg+" fnorm={0}".format(self.fnorm))
             gnorm = 0.
             if self.fnorm != 0:
                 for j in range(n):
@@ -1178,7 +1178,7 @@ class mpfit:
             # Test for convergence of the gradient norm
             if gnorm <= gtol:
                 self.status = 4
-                log.debug("gnorm={0} gtol={1} BREAK with status 4"
+                log.log(5, "gnorm={0} gtol={1} BREAK with status 4"
                           .format(gnorm,gtol))
                 break
             if maxiter == 0:
@@ -1273,8 +1273,8 @@ class mpfit:
                     delta = numpy.min([delta,pnorm])
 
                 self.params[ifree] = wa2
-                log.debug("after parameter setting: wa2={0}".format(wa2))
-                log.debug("after parameter setting: params={0}".format(self.params))
+                log.log(5, "after parameter setting: wa2={0}".format(wa2))
+                log.log(5, "after parameter setting: params={0}".format(self.params))
 
                 # Evaluate the function at x+p and calculate its norm
                 mperr = 0
@@ -1282,12 +1282,12 @@ class mpfit:
                 [self.status, wa4] = self.call(fcn, self.params, functkw)
                 if self.status < 0:
                     self.errmsg = 'WARNING: premature termination by "'+fcn+'"'
-                    log.debug(self.errmsg)
+                    log.log(5, self.errmsg)
                     return
                 fnorm1 = self.enorm(wa4)
-                log.debug("params={0}".format(self.params))
-                log.debug("fnorm={0}".format(self.fnorm))
-                log.debug("fnorm1={0}".format(fnorm1))
+                log.log(5, "params={0}".format(self.params))
+                log.log(5, "fnorm={0}".format(self.fnorm))
+                log.log(5, "fnorm1={0}".format(fnorm1))
                 #print("wa4={0}".format(wa4))
 
                 # Compute the scaled actual reduction
@@ -1295,7 +1295,7 @@ class mpfit:
                 actred = -1.
                 if (0.1 * fnorm1) < self.fnorm:
                     actred = - (fnorm1/self.fnorm)**2 + 1.
-                log.debug("actred={0}".format(actred))
+                log.log(5, "actred={0}".format(actred))
 
                 # Compute the scaled predicted reduction and the scaled directional
                 # derivative
@@ -1330,7 +1330,7 @@ class mpfit:
                         delta = pnorm/.5
                         par = .5*par
 
-                log.debug("ratio={0}".format(ratio))
+                log.log(5, "ratio={0}".format(ratio))
 
                 # Test for successful iteration
                 if ratio >= 0.0001:
@@ -1341,7 +1341,7 @@ class mpfit:
                     xnorm = self.enorm(wa2)
                     self.fnorm = fnorm1
                     self.niter = self.niter + 1
-                log.debug("at 'test for successful iteration' ratio={0} "
+                log.log(5, "at 'test for successful iteration' ratio={0} "
                           "fnorm={1}".format(ratio,self.fnorm))
 
                 # Tests for convergence
@@ -1354,7 +1354,7 @@ class mpfit:
                      and (0.5 * ratio <= 1) and (self.status == 2):
                      self.status = 3
                 if self.status != 0:
-                    log.debug("BREAK with status: {0}".format(self.status))
+                    log.log(5, "BREAK with status: {0}".format(self.status))
                     break
 
                 # Tests for termination and stringent tolerances
@@ -1368,12 +1368,12 @@ class mpfit:
                 if gnorm <= machep:
                     self.status = 8
                 if self.status != 0:
-                    log.debug("BREAK with status: {0}".format(self.status))
+                    log.log(5, "BREAK with status: {0}".format(self.status))
                     break
 
                 # End of inner loop. Repeat if iteration unsuccessful
                 if ratio >= 0.0001:
-                    log.debug("BREAK with ratio = {1} status: {0}".format(self.status, ratio))
+                    log.log(5, "BREAK with ratio = {1} status: {0}".format(self.status, ratio))
                     break
 
                 # Check for over/underflow
@@ -1382,18 +1382,18 @@ class mpfit:
                     errmsg = ('''ERROR: parameter or function value(s) have become
                         'infinite; check model function for over- 'and underflow''')
                     self.status = -16
-                    log.debug("BREAK with status: {0} errmsg={1}".format(self.status, errmsg))
+                    log.log(5, "BREAK with status: {0} errmsg={1}".format(self.status, errmsg))
                     break
                 #wh = where(finite(wa1) EQ 0 OR finite(wa2) EQ 0 OR finite(x) EQ 0, ct)
                 #if ct GT 0 OR finite(ratio) EQ 0 then begin
 
             if self.status != 0:
-                log.debug("BREAK with status: {0}".format(self.status))
+                log.log(5, "BREAK with status: {0}".format(self.status))
                 break
         # End of outer loop.
 
         catch_msg = 'in the termination phase'
-        log.debug(catch_msg)
+        log.log(5, catch_msg)
         # Termination, either normal or user imposed.
         if len(self.params) == 0:
             return
@@ -1406,13 +1406,13 @@ class mpfit:
             [status, fvec] = self.call(fcn, self.params, functkw)
             catch_msg = 'in the termination phase'
             self.fnorm = self.enorm(fvec)
-        log.debug("After iterations, status={1} fnorm={0}".format(self.fnorm, self.status))
+        log.log(5, "After iterations, status={1} fnorm={0}".format(self.fnorm, self.status))
 
         if (self.fnorm is not None) and (fnorm1 is not None):
             self.fnorm = numpy.max([self.fnorm, fnorm1])
             self.fnorm = self.fnorm**2.
 
-        log.debug("After iterations, fnorm={0}".format(self.fnorm))
+        log.log(5, "After iterations, fnorm={0}".format(self.fnorm))
 
         self.covar = None
         self.perror = None
@@ -1464,7 +1464,7 @@ class mpfit:
                 iterstop=None, parinfo=None, format=None, pformat='%.10g',
                 dof=1):
 
-        log.debug('Entering defiter...')
+        log.log(5, 'Entering defiter...')
         if quiet:
             return
         if fnorm is None:
@@ -1511,7 +1511,7 @@ class mpfit:
 
     # Procedure to parse the parameter values in PARINFO, which is a list of dictionaries
     def parinfo(self, parinfo=None, key='a', default=None, n=0):
-        log.debug('Entering parinfo...')
+        log.log(5, 'Entering parinfo...')
         if (n == 0) and (parinfo is not None):
             n = len(parinfo)
         if n == 0:
@@ -1538,7 +1538,7 @@ class mpfit:
     # Call user function or procedure, with _EXTRA or not, with
     # derivatives or not.
     def call(self, fcn, x, functkw, fjac=None):
-        log.debug('Entering call with x={0}...'.format(x))
+        log.log(5, 'Entering call with x={0}...'.format(x))
         if self.qanytied:
             x = self.tie(x, self.ptied)
         self.nfev = self.nfev + 1
@@ -1566,7 +1566,7 @@ class mpfit:
                dside=None, epsfcn=None, autoderivative=1, functkw=None,
                xall=None, ifree=None, dstep=None):
 
-        log.debug('Entering fdjac2...')
+        log.log(5, 'Entering fdjac2...')
         machep = self.machar.machep
         if epsfcn is None:
             epsfcn = machep
@@ -1619,7 +1619,7 @@ class mpfit:
         # if relative step is given, use that
         # DSTEP includes the fixed parameters
         if len(dstep) > 0:
-            log.debug("Using relative step size dstep={0}".format(dstep))
+            log.log(5, "Using relative step size dstep={0}".format(dstep))
             dstepi = dstep[ifree]
             wh = (numpy.nonzero(dstepi > 0))[0]
             if len(wh) > 0:
@@ -1628,7 +1628,7 @@ class mpfit:
         # In case any of the step values are zero
         h[h == 0] = eps
 
-        log.debug("In fdjac2, epsilon={0}, m={1}, n={2}, and h={3}"
+        log.log(5, "In fdjac2, epsilon={0}, m={1}, n={2}, and h={3}"
                   .format(eps, m, n, h))
 
         # Reverse the sign of the step if we are up against the parameter
@@ -1803,7 +1803,7 @@ class mpfit:
 
     def qrfac(self, a, pivot=0):
 
-        log.debug('Entering qrfac...')
+        log.log(5, 'Entering qrfac...')
         machep = self.machar.machep
         sz = a.shape
         m = sz[0]
@@ -1957,7 +1957,7 @@ class mpfit:
     #
 
     def qrsolv(self, r, ipvt, diag, qtb, sdiag):
-        log.debug('Entering qrsolv...')
+        log.log(5, 'Entering qrsolv...')
         sz = r.shape
         # not used m = sz[0]
         n = sz[1]
@@ -2128,7 +2128,7 @@ class mpfit:
 
     def lmpar(self, r, ipvt, diag, qtb, delta, x, sdiag, par=None):
 
-        log.debug('Entering lmpar...')
+        log.log(5, 'Entering lmpar...')
         dwarf = self.machar.minnum
         machep = self.machar.machep
         sz = r.shape

pyspeckit/spectrum/fitters.py
@@ -580,7 +580,8 @@ class Specfit(interactive.Interactive):
             # otherwise npars will disagree, which causes problems if
             # renormalization happens
             self.fitter.vheight = False
-            self.fitter._make_parinfo()
+            self.fitter.npeaks = self.npeaks
+            self.fitter._make_parinfo(npeaks=self.npeaks)
 
         # add kwargs to fitkwargs
         self.fitkwargs.update(kwargs)
@@ -616,7 +617,9 @@ class Specfit(interactive.Interactive):
         if parinfo is not None:
             self._validate_parinfo(parinfo, mode='fix')
         else:
-            pinf, _ = self.fitter._make_parinfo(parvalues=guesses, **self.fitkwargs)
+            pinf, _ = self.fitter._make_parinfo(parvalues=guesses,
+                                                npeaks=self.npeaks,
+                                                **self.fitkwargs)
             new_guesses = self._validate_parinfo(pinf, 'guesses')
             if any((x!=y) for x,y in zip(guesses, new_guesses)):
                 warn("Guesses have been changed from {0} to {1}"

pyspeckit/spectrum/interactive.py
@@ -382,11 +382,11 @@ class Interactive(object):
             * None: No exclusion
         """
         if debug or self._debug:
-            print "selectregion kwargs: ",kwargs," use_window_limits: ",use_window_limits," reset: ",reset," xmin: ",xmin, " xmax: ",xmax
+            log.info(map(str, ("selectregion kwargs: ",kwargs," use_window_limits: ",use_window_limits," reset: ",reset," xmin: ",xmin, " xmax: ",xmax)))
 
         if xmin is not None and xmax is not None:
             if verbose or debug or self._debug:
-                print "Setting xmin,xmax from keywords %g,%g" % (xmin,xmax)
+                log.info("Setting xmin,xmax from keywords %g,%g" % (xmin,xmax))
             if xtype.lower() in ('wcs',) or xtype in pyspeckit.spectrum.units.xtype_dict:
                 self.xmin = numpy.floor(self.Spectrum.xarr.x_to_pix(xmin))
                 # End-inclusive!
@@ -415,12 +415,19 @@ class Interactive(object):
             # this feels sketchy to me, but if you don't do this the plot will not be edge-inclusive
             # that means you could do this reset operation N times to continuously shrink the plot
             self.xmax += 1
-            if debug or self._debug: print "Including all plotted area (as defined by [plotter.xmin=%f,plotter.xmax=%f]) for fit" % (self.Spectrum.plotter.xmin,self.Spectrum.plotter.xmax)
-            if debug or self._debug: print "Including self.xmin:self.xmax = %f:%f (and excluding the rest)" % (self.xmin,self.xmax)
+            if debug or self._debug:
+                log.debug("Including all plotted area (as defined by "
+                          "[plotter.xmin={0}, plotter.xmax={1}]) for "
+                          "fit".format(self.Spectrum.plotter.xmin,
+                                       self.Spectrum.plotter.xmax))
+                log.debug("Including self.xmin:self.xmax = {0}:{1}"
+                          " (and excluding the rest)".format(self.xmin,
+                                                             self.xmax))
             self.includemask[self.xmin:self.xmax] = True
         else:
-            if verbose: log.info("Left region selection unchanged."
-                                 "  xminpix, xmaxpix: %i,%i" % (self.xmin,self.xmax))
+            if verbose:
+                log.info("Left region selection unchanged."
+                         "  xminpix, xmaxpix: %i,%i" % (self.xmin,self.xmax))
         
         if self.xmin == self.xmax:
             # Reset if there is no fitting region

pyspeckit/spectrum/models/model.py
@@ -148,6 +148,7 @@ class SpectralModel(fitter.SimpleFitter):
             raise ValueError("parvalues and params both specified; they're redundant so that's not allowed.")
         elif params is not None and parvalues is None:
             parvalues = params
+        log.debug("Parvalues = {0}, npeaks = {1}".format(parvalues, npeaks))
 
         if parnames is not None: 
             self.parnames = parnames
@@ -205,7 +206,7 @@ class SpectralModel(fitter.SimpleFitter):
         else:
             self.parinfo = []
 
-        if debug: log.debug("After VHEIGHT parse len(parinfo): %i   vheight: %s" % (len(self.parinfo), vheight))
+        log.debug("After VHEIGHT parse len(parinfo): %i   vheight: %s" % (len(self.parinfo), vheight))
 
 
         # this is a clever way to turn the parameter lists into a dict of lists
@@ -234,7 +235,7 @@ class SpectralModel(fitter.SimpleFitter):
             for jj in xrange(self.npeaks)
             for ii in xrange(self.npars) ] # order matters!
 
-        if debug: log.debug("After Generation step len(parinfo): %i   vheight: %s" % (len(self.parinfo), vheight))
+        log.debug("After Generation step len(parinfo): %i   vheight: %s" % (len(self.parinfo), vheight))
 
         if debug > True: import pdb; pdb.set_trace()
 
@@ -261,6 +262,8 @@ class SpectralModel(fitter.SimpleFitter):
             if par.parname.lower().strip('0123456789') in ('amplitude','amp'):
                 par.scaleable = True
 
+        log.debug("Parinfo has been set: {0}".format(self.parinfo))
+
         return self.parinfo, kwargs
 
     def n_modelfunc(self, pars=None, debug=False, **kwargs):
@@ -275,11 +278,9 @@ class SpectralModel(fitter.SimpleFitter):
                 partemp._from_Parameters(pars)
                 pars = partemp
             except AttributeError:
-                if debug: 
-                    log.debug("Reading pars as LMPar failed.")
-                    if debug > 1:
-                        import pdb; pdb.set_trace()
-                pass
+                log.log(5, "Reading pars {0} as LMPar failed.".format(pars))
+                if debug > 1:
+                    import pdb; pdb.set_trace()
         if hasattr(pars,'values'):
             # important to treat as Dictionary, since lmfit params & parinfo both have .items
             parnames,parvals = zip(*pars.items())
@@ -287,8 +288,8 @@ class SpectralModel(fitter.SimpleFitter):
             parvals = [p.value for p in parvals]
         else:
             parvals = list(pars)
-        if debug:
-            log.debug("pars to n_modelfunc: {0}, parvals:{1}".format(pars, parvals))
+
+        log.debug("pars to n_modelfunc: {0}, parvals:{1}".format(pars, parvals))
         def L(x):
             v = np.zeros(len(x))
             if self.vheight:
@@ -325,7 +326,7 @@ class SpectralModel(fitter.SimpleFitter):
             kwargs = {}
             kwargs.update(self.modelfunc_kwargs)
 
-            if debug: log.debug("Pars, kwarg keys: {0},{1}".format(p,kwargs.keys()))
+            log.debug("Pars, kwarg keys: {0},{1}".format(p,kwargs.keys()))
             if err is None:
                 return (y-self.n_modelfunc(p,**kwargs)(x))
             else:
@@ -383,13 +384,11 @@ class SpectralModel(fitter.SimpleFitter):
 
         if parinfo is None:
             parinfo, kwargs = self._make_parinfo(debug=debug, **kwargs)
-            if debug:
-                log.debug(parinfo)
+            log.debug("Parinfo created from _make_parinfo: {0}".format(parinfo))
 
         LMParams = parinfo.as_Parameters()
-        if debug:
-            log.debug("LMParams: "+"\n".join([repr(p) for p in LMParams.values()]))
-            log.debug("parinfo:  {0}".format(parinfo))
+        log.debug("LMParams: "+"\n".join([repr(p) for p in LMParams.values()]))
+        log.debug("parinfo:  {0}".format(parinfo))
         minimizer = lmfit.minimize(self.lmfitfun(xax,np.array(data),err,debug=debug),LMParams,**kwargs)
         if not quiet:
             log.info("There were %i function evaluations" % (minimizer.nfev))
@@ -398,9 +397,8 @@ class SpectralModel(fitter.SimpleFitter):
 
         self.LMParams = LMParams
         self.parinfo._from_Parameters(LMParams)
-        if debug:
-            log.debug(LMParams)
-            log.debug(parinfo)
+        log.debug("LMParams: {0}".format(LMParams))
+        log.debug("parinfo: {0}".format(parinfo))
 
         self.mp = minimizer
         self.mpp = self.parinfo.values
@@ -454,7 +452,7 @@ class SpectralModel(fitter.SimpleFitter):
         if parinfo is None:
             parinfo, kwargs = self._make_parinfo(debug=debug, **kwargs)
         else:
-            if debug: log.debug("Using user-specified parinfo dict")
+            log.debug("Using user-specified parinfo dict")
             # clean out disallowed kwargs (don't want to pass them to mpfit)
             #throwaway, kwargs = self._make_parinfo(debug=debug, **kwargs)
 
@@ -484,9 +482,8 @@ class SpectralModel(fitter.SimpleFitter):
                              "not allowed as negative errors are not "
                              "meaningful in the optimization process.")
 
-        if debug:
-            for p in parinfo: log.debug( p )
-            log.debug( "\n".join(["%s %i: tied: %s value: %s" % (p['parname'],p['n'],p['tied'],p['value']) for p in parinfo]) )
+        for p in parinfo: log.debug( p )
+        log.debug( "\n".join(["%s %i: tied: %s value: %s" % (p['parname'],p['n'],p['tied'],p['value']) for p in parinfo]) )
 
         mp = mpfit(self.mpfitfun(xax,data,err),parinfo=parinfo,quiet=quiet,debug=debug,**kwargs)
         mpp = mp.params
@@ -519,8 +516,7 @@ class SpectralModel(fitter.SimpleFitter):
         self.mpperr = self.parinfo.errors
         self.mppnames = self.parinfo.names
         self.model = self.n_modelfunc(self.parinfo,**self.modelfunc_kwargs)(xax)
-        if debug:
-            log.debug("Modelpars: {0}".format(self.mpp))
+        log.debug("Modelpars: {0}".format(self.mpp))
         if np.isnan(chi2):
             if debug:
                 raise ValueError("Error: chi^2 is nan")
@@ -579,7 +575,7 @@ class SpectralModel(fitter.SimpleFitter):
 
         label_list = []
         for (value, error, varname, fixed, varnumber) in loop_list:
-            if debug: log.debug("".join(value, error, varname, fixed, varnumber))
+            log.debug(", ".join([str(x) for x in (value, error, varname, fixed, varnumber)]))
             if fixed or error==0:
                 label = ("$%s(%i)$=%8s" % (varname,varnumber,
                         Decimal("%g" % value).quantize( Decimal("%0.6g" % (value)) )))