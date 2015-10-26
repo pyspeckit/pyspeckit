f4557b1c0f6ba5d144faab5015414f90281ff5ee
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-10-26 20:01:02 +0100

    2to3 some examples

--- examples/ammonia_vtau_fit_example.py
+++ examples/ammonia_vtau_fit_example.py
@@ -67,8 +67,8 @@ sp.specfit(negamp=False, guesses='moments')
 # Save the fit...
 sp.plotter.figure.savefig('nh3_gaussfit.png')
 # and print some information to screen
-print "Guesses: ", sp.specfit.guesses
-print "Best fit: ", sp.specfit.modelpars
+print("Guesses: ", sp.specfit.guesses)
+print("Best fit: ", sp.specfit.modelpars)
 
 # Run the ammonia spec fitter with a reasonable guess
 # Since we only have a single line (1-1), the kinetic temperature is
@@ -85,8 +85,8 @@ sp.specfit(fittype='ammonia_tau',
 sp.specfit.plotresiduals()
 
 sp.plotter.figure.savefig('nh3_ammonia_vtau_fit.png')
-print "Guesses: ", sp.specfit.guesses
-print "Best fit: ", sp.specfit.modelpars
+print("Guesses: ", sp.specfit.guesses)
+print("Best fit: ", sp.specfit.modelpars)
 
 
 # re-plot zoomed in
