07551f7308a9e7c1dae7f66369f1faf5717580dd
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-20 17:03:13 +0100

    fix syntax err

--- pyspeckit/cubes/SpectralCube.py
+++ pyspeckit/cubes/SpectralCube.py
@@ -1146,7 +1146,7 @@ class Cube(spectrum.Spectrum):
             self.specfit.fitter = sp.specfit.fitter
         except Exception as ex:
             log.error("Fitting the pixel at location {0} failed with error: {1}.  "
-                      "Try setting _temp_fit_loc to a valid pixel".format(_temp_fit_loc, ex)))
+                      "Try setting _temp_fit_loc to a valid pixel".format(_temp_fit_loc, ex))
 
         self.specfit.fittype = sp.specfit.fittype
         self.specfit.parinfo = sp.specfit.parinfo
