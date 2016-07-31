0c73a29a31e923b8f418f2e1cc18298946ef608a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-07-31 19:45:58 +0200

    catch another warning

--- pyspeckit/cubes/tests/test_cubetools.py
+++ pyspeckit/cubes/tests/test_cubetools.py
@@ -247,8 +247,10 @@ def test_noerror_cube(cubefile='test.fits'):
                     start_from_point=(4,4),
                    )
     assert "If signal_cut is set" in str(w[-1].message)
-
     assert not np.all(spc.has_fit)
 
-    spc.fiteach(fittype='gaussian', guesses=[0.7,0.5,0.8], signal_cut=0)
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('default')
+        spc.fiteach(fittype='gaussian', guesses=[0.7,0.5,0.8], signal_cut=0)
+    assert "If signal_cut is set" in str(w[-1].message)
     assert np.all(spc.has_fit)
