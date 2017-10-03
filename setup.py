db932fc8fb16c92a5ff31e6b0a413cbf2ac56d37
Brigitta Sipocz <bsipocz@gmail.com>
2017-10-03 14:52:22 +0100

    Backporting #275

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ else:
 builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
 # VERSION should be PEP440 compatible (http://www.python.org/dev/peps/pep-0440)
-VERSION = metadata.get('version', '0.0.dev')
+VERSION = metadata.get('version', '0.0.dev0')
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
