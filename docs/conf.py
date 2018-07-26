95f31fc49cbcd70a309a469e471fd80cfdba882c
Travis CI <travis@travis.ci>
2018-07-26 16:04:11 +0000

    Update rendered version to 60051dd49d07201328cc96a776558c6b1dab0aaf

--- docs/conf.py
+++ docs/conf.py
@@ -107,10 +107,9 @@ release = package.__version__
 #html_theme = None
 
 
-# Please update these texts to match the name of your package.
 html_theme_options = {
-    'logotext1': 'package',  # white,  semi-bold
-    'logotext2': '-template',  # orange, light
+    'logotext1': 'packagename',  # white,  semi-bold
+    'logotext2': '',  # orange, light
     'logotext3': ':docs'   # white,  light
     }
 
