8121465e891b0d01aa050c11a84bd23058ce678e
Travis CI <travis@travis.ci>
2019-06-08 21:34:31 +0000

    Update rendered version to f92c94967b6faae518bc9edaf114e652b3c61cde

--- packagename/__init__.py
+++ packagename/__init__.py
@@ -24,4 +24,4 @@ if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
 
 if not _ASTROPY_SETUP_:   # noqa
     # For egg_info test builds to pass, put package imports here.
-    from .example_mod import *   #noqa
+    from .example_mod import *   # noqa
