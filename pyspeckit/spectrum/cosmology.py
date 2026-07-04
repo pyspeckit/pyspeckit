Fix long-broken legacy readers, writers, measurements, units, and cosmology
These code paths crashed unconditionally under Python 3 / modern numpy /
scipy / h5py.  Graduated from the fix-legacy-io-plotting audit branch;
only fixes not already merged via #430/#437/#450 are included.

Readers:
- hdf5_reader.py: replace removed h5py Dataset.value with [()]; close the
  file after reading
- read_class.py: replace ipdb.set_trace with ValueError when NCHAN is
  missing; fix undefined 'hdr' in _spectrum_from_header; don't treat an
  index-0-only selection as empty in get_spectra; append the final scan's
  spectrumlist in class_to_obsblocks; fix LazyItem __iter__ (proper
  generator over sphdr) and slice handling (key.stop, not key.end)
- read_class_lmv.py: compare GILDAS-IMAGE magic against bytes
- galex.py: use classes.Spectrum/classes.ObsBlock (bare names were
  NameErrors); fix SpectroscopicAxis kwarg (unit='angstrom')
- alfalfa.py: list-wrap zip() results before indexing
- tspec_reader.py: drop the long-removed xtype=/positional-units
  SpectroscopicAxis call; map IRAF plural unit names
  (angstroms/microns/nanometers) to the singular forms astropy requires;
  define errspec in the IRAF-reduced branch (previously NameError);
  verified against a real TripleSpec file (BD60596_merged.fits)

Writers:
- txt_writer.py: list-wrap zip(); guard empty/None modelcomponents; use
  specfit.xmin/xmax (gx1/gx2 no longer exist)
- hdf5_writer.py: actually open the file in the overwrite=False branch
  (previously NameError: 'f')

Measurements:
- measurements.py: list-wrap zip() subscripts; integer division in
  separate(); fix misplaced parenthesis in np.any() comparison; remove
  per-component double-counting in compute_luminosity (compute_flux
  already sums components)

Units:
- units.py: repair SmartCaseNoSpaceDict.has_key (dict.has_key__ never
  existed), setdefault/pop default forwarding, and update/fromkeys
  __setitem__ calls with a spurious self argument

Cosmology:
- cosmolog

pyspeckit/spectrum/cosmology.py
@@ -18,7 +18,7 @@ from __future__ import print_function
 import numpy as np
 
 try:
-    from scipy.integrate import romberg
+    from scipy.integrate import quad
     scipyOK=True
 except:
     scipyOK=False
@@ -74,9 +74,9 @@ class Cosmology:
         Integrand = lambda z: (1.0 / (1.0 + z) / self.EvolutionFunction(z))
         
         if scipyOK:
-            return (romberg(Integrand, z_i, z_f) / self.HubbleParameterNow)    
-        else: 
-            return 0
+            return (quad(Integrand, z_i, z_f)[0] / self.HubbleParameterNow)
+        else:
+            raise ImportError("scipy is required to compute the lookback time.")
         
     def TimeToRedshiftConverter(self, z_i, dt):
         """
@@ -93,9 +93,9 @@ class Cosmology:
         
         integrand = lambda z: 1. / self.EvolutionFunction(z)
         if scipyOK:
-            return romberg(integrand, z_i, z_f) * c / self.HubbleParameterNow / cm_per_mpc
-        else: 
-            return 0
+            return quad(integrand, z_i, z_f)[0] * c / self.HubbleParameterNow / cm_per_mpc
+        else:
+            raise ImportError("scipy is required to compute the comoving radial distance.")
         
     def LuminosityDistance(self, z_f):
         """

pyspeckit/spectrum/measurements.py
@@ -209,7 +209,7 @@ class Measurements(object):
 
         # If lines have multiple components (i.e. spacing much closer than ref lines),
         # delete them from ID list.
-        if np.any(self.odiff) < self.rdmin:
+        if np.any(self.odiff < self.rdmin):
             where = np.ravel(np.argwhere(self.odiff < self.rdmin))
             odiff = np.delete(self.odiff, where)
             multi = True
@@ -255,7 +255,7 @@ class Measurements(object):
                         self.IDresults.append(result)
 
         # Pick best solution
-        best = np.argmin(zip(*self.IDresults)[0])  # Location of best solution
+        best = np.argmin(list(zip(*self.IDresults))[0])  # Location of best solution
         ALLloc = []                                # x-values of best fit lines in reference dictionary
 
         # Determine indices of matched reference lines
@@ -278,14 +278,14 @@ class Measurements(object):
             mpars = self.modelpars.copy()
             merrs = self.modelerrs.copy()
             for line in self.lines:
-                wavelengths = zip(*mpars)[1]
-                i = np.argmin(np.abs(zip(*mpars)[1] - self.lines[line]['modelpars'][1]))
+                wavelengths = np.array(list(zip(*mpars))[1])
+                i = np.argmin(np.abs(wavelengths - self.lines[line]['modelpars'][1]))
                 mpars = np.delete(mpars, i, 0)
                 merrs = np.delete(merrs, i, 0)
 
             # Loop over unmatched modelpars/errs, find name of unmatched line, extend corresponding dict entry
             if self.miscline is None:
-                for i, x in enumerate(zip(*mpars)[1]):
+                for i, x in enumerate(list(zip(*mpars))[1]):
                     self.lines['unknown%i' % i] = {}
                     self.lines['unknown%i' % i]['modelpars'] = mpars[i]
                     self.lines['unknown%i' % i]['modelerrs'] = merrs[i]
@@ -294,7 +294,7 @@ class Measurements(object):
             else:
                 print(self.miscline)
                 for i, miscline in enumerate(self.miscline):
-                    for j, x in enumerate(zip(*mpars)[1]):
+                    for j, x in enumerate(list(zip(*mpars))[1]):
                         if abs(x - miscline['wavelength']) < self.misctol:
                             name = miscline['name']
                         else:
@@ -332,9 +332,9 @@ class Measurements(object):
             modpars = self.lines[key]['modelpars']
             moderrs = self.lines[key]['modelerrs']
             if len(modpars) > 3:
-                modpars2d = np.reshape(modpars, (len(modpars) / 3, 3))
-                moderrs2d = np.reshape(moderrs, (len(moderrs) / 3, 3))
-                sigma = zip(*modpars2d)[2]
+                modpars2d = np.reshape(modpars, (len(modpars) // 3, 3))
+                moderrs2d = np.reshape(moderrs, (len(moderrs) // 3, 3))
+                sigma = list(zip(*modpars2d))[2]
                 minsigma = min(np.abs(sigma))
                 i_narrow = list(np.abs(sigma)).index(minsigma)
             else:
@@ -385,10 +385,8 @@ class Measurements(object):
         Determine luminosity of line (need distance and flux units).
         """
 
-        lum = 0
-        niter = (len(pars) / 3)
-        for i in xrange(int(niter)):
-            lum += self.compute_flux(pars) * 4. * np.pi * self.d**2
+        # compute_flux already sums over all components
+        lum = self.compute_flux(pars) * 4. * np.pi * self.d**2
         return lum
 
     def compute_fwhm(self, pars):
@@ -406,8 +404,8 @@ class Measurements(object):
             start = list(zip(*pars2d))[1][0]                    # start at central wavelength of first component
 
             # If the centroids are exactly the same for all components, we know the peak, and peak position
-            if np.allclose(zip(*pars2d)[1], atol):
-                fmax = np.sum(zip(*pars2d)[0])
+            if np.allclose(list(zip(*pars2d))[1], atol):
+                fmax = np.sum(list(zip(*pars2d))[0])
 
             # Otherwise, we have to figure out where the multicomponent peak is
             else:

pyspeckit/spectrum/readers/galex.py
@@ -42,11 +42,11 @@ def read_galex(fitsfilename, orderselection='obj'):
 
     # wavelength in angstroms
     wavelength = bintable.zero + np.arange(len(bintable.disp)) * bintable.disp
-    xaxis = units.SpectroscopicAxis(wavelength,units='angstroms')
+    xaxis = units.SpectroscopicAxis(wavelength,unit='angstrom')
 
-    splist = [Spectrum(data=bintable[orderselection][:,ii], error=bintable[orderselection+"err"][:,ii], xarr=xaxis, header=ff[1].header)
+    splist = [classes.Spectrum(data=bintable[orderselection][:,ii], error=bintable[orderselection+"err"][:,ii], xarr=xaxis, header=ff[1].header)
             for ii in xrange(bintable[orderselection].shape[1])]
 
-    galexblock = ObsBlock(splist)
+    galexblock = classes.ObsBlock(splist)
 
     return galexblock

pyspeckit/spectrum/readers/hdf5_reader.py
@@ -27,13 +27,13 @@ def open_hdf5(filename, xaxkey = 'xarr', datakey = 'data', errkey = 'error'):
 
     f = h5py.File(filename, 'r')
     
-    try: xarr = f[xaxkey].value
+    try: xarr = f[xaxkey][()]
     except KeyError: print('Dataset \'%s\' not found.' % xaxkey)
-    
-    try: data = f[datakey].value
+
+    try: data = f[datakey][()]
     except KeyError: print('Dataset \'%s\' not found.' % datakey)
 
-    try: error = f[errkey].value
+    try: error = f[errkey][()]
     except KeyError: 
         print('Dataset \'%s\' not found.  Assuming uniform errors.' % errkey)
         error = np.ones_like(data)
@@ -66,9 +66,11 @@ def open_hdf5(filename, xaxkey = 'xarr', datakey = 'data', errkey = 'error'):
     except KeyError: 
         ytype = datakey            
         
-    XAxis = units.SpectroscopicAxis(xarr, xunits)    
+    f.close()
+
+    XAxis = units.SpectroscopicAxis(xarr, xunits)
 
     header = {'xunits': xunits, 'xtype': xtype, 'yunits': yunits, 'ytype': ytype}
-    
+
     return data, error, XAxis, header
 